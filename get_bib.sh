377ffa27d4dc701a578fa01918dc8b7b7f922236
Lars Vilhuber <lars.vilhuber@cornell.edu>
2016-11-18 12:13:44 -0500

    Accept changes

--- get_bib.sh
+++ get_bib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://www.ncrn.info/documents/bibliographies/export/bibtex > Biblio-Bibtex.bib
+curl https://www2.vrdc.cornell.edu/news/wp-content/papercite-data/bib/ssb-data.bib > ssb-data.bib
