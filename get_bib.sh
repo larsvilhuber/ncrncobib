5f6902b5962a759654b0fe2bb82b3341989967bc
Lars Vilhuber <lars.vilhuber@cornell.edu>
2016-11-18 13:07:37 -0500

    Sample bibliography

--- get_bib.sh
+++ get_bib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl https://www2.vrdc.cornell.edu/news/wp-content/papercite-data/bib/ssb-data.bib > ssb-data.bib
+curl https://www2.vrdc.cornell.edu/news/wp-content/papercite-data/bib/ssb-documents.bib > ssb-documents.bib
